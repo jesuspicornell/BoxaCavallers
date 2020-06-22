2274ccdfc7dde39cd4f8a2c28a80691c06098c9a
Jesus Picornell <jesus.picornell@gmail.com>
2020-06-22 12:58:27 +0200

    A veure si ara funciona

---
+++ src/main/java/net/xaviersala/combatcavallers/App.java
@@ -0,0 +1,31 @@
+package net.xaviersala.combatcavallers;
+
+import net.xaviersala.combatcavallers.combat.Ring;
+import net.xaviersala.combatcavallers.lluitador.LLuitadorRandom;
+
+/**
+ * Programa que simula el funcionament de la 'boxa entre cavallers'.
+ *
+ * @author xavier
+ *
+ */
+public class App {
+
+  public static void main(String[] args) {
+	  
+	  // Aquest seria la versió 2 de les contrasenyes, no he desenvolupat el codi
+	  // Feta amb git flow
+
+    // Creo dos boxejadors
+    LLuitadorRandom lluitador1 = new LLuitadorRandom("MatxacaCaps");
+    LLuitadorRandom lluitador2 = new LLuitadorRandom("DestrossaPilotes");
+
+    // Poso els dos boxejadors en el combat
+    Ring combat = new Ring();
+
+    combat.EntradaLluitadors(lluitador1, lluitador2);
+    combat.Lluiteu();
+
+  }
+
+}
